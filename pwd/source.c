d0cee2938efdd1809ed7be250a010ef386cf9bba
Justfindingsomethinginglife <golfgamertv192@gmail.com>
2023-05-30 19:33:31 +0700

    We've finally (fix) my error in the code

--- pwd/source.c
+++ pwd/source.c
@@ -11,8 +11,8 @@
 
 //------------------------------------------------------------------------------------------------
 
-int STACK_ADDR_MAX = 0x1FF;
-int STACK_ADDR_MIN = 0x100;
+#define STACK_ADDR_MAX = 0x1FF;
+#define STACK_ADDR_MIN = 0x100;
 //------------------------------------------------------------------------------------------------
 
 //                                                     STACK
@@ -38,7 +38,7 @@ int isEmpty(uint8_t *stack){
 int isFull(uint8_t *stack,int pos,int size){
 	if(isEmpty(stack)){return -1;}
 
-	if(pos = size){
+	if(pos == size){
 		return 0;
 	}
 	else{
@@ -48,6 +48,30 @@ int isFull(uint8_t *stack,int pos,int size){
 
 int peek(uint8_t *stack,int pos){
 	return stack[pos];
+}
+
+int push(uint8_t *stack,int *pos,int size,uint8_t val){
+	if(isEmpty(stack)){
+		return -1;
+	}
+
+	if(!isFull(stack,*pos,size)){
+		return 2;
+	}
+	else{
+		stack[(*pos)+1] = val;
+	}
+	(*pos)++;
+
+}
+
+int pop(uint8_t *stack,int *pos,int size){
+	if(isEmpty(stack)){
+		return -1;
+	}
+	else{
+		(*pos)--;
+	}
 }
 
 
@@ -65,23 +89,12 @@ uint16_t initialize(uint8_t PCL,uint8_t PCH,uint16_t PC){
 
 
 int main(){
-	int pos=0;
+	int pos=-1;
 	int size = 256;
 	uint8_t *STACK_256 = (uint8_t*)malloc(256*sizeof(uint8_t));
-	
-	for (int j = 0; j < 2; ++j) {
-		pos++;
-		STACK_256[j] = j + 1;
-        }
-	for (int k = 0; k < pos; ++k) {
-
-        	printf("%d, ", STACK_256[k]);
-
-    	}
-
-	printf("%d",peek(STACK_256,1));
-
-        PC = initialize(PCL,PCH,PC);
-	
 	return 0;
 }
+
+
+
+

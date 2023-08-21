21a26707ec9189667a1fe8407f8d5c4248318c46
Justfindingsomethinginlife <golfgamertv192@gmail.com>
2023-08-21 18:40:34 +0700

    updated OpCode

--- Main/main.c
+++ Main/main.c
@@ -8,6 +8,7 @@ int main(){
 	uint8_t *stack_256 = (uint8_t*)malloc(sizeof(uint8_t)*256);
 
 	printf("%d",IRX);
+	printf("%d",Accumulator);
 	Instruction_Decode(0xC8,&PSR,&Accumulator,&IRX,&IRY,&DL,&PCH,&PCL,&PC,stack_256);
 	printf("%d",IRX);
 	Instruction_Decode(0x8A,&PSR,&Accumulator,&IRX,&IRY,&DL,&PCH,&PCL,&PC,stack_256);
